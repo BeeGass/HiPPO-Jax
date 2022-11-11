cd9307244dc3e9ac7c951fe2247c538e22d07b38
Bryan Gass <bryank123@live.com>
2022-11-11 15:47:31 -0500

    changing skelaton of deep RNN for remodeling

--- src/models/sequence/rnn/rnn2.py
+++ src/models/sequence/rnn/rnn2.py
@@ -22,7 +22,7 @@ from src.models.hippo.hippo import HiPPO
 
 # TODO: refer to https://github.com/deepmind/dm-haiku/blob/main/haiku/_src/recurrent.py#L714-L762
 # also refer to https://dm-haiku.readthedocs.io/en/latest/api.html?highlight=DeepRNN#deeprnn
-class _DeepRNN(RNNCellBase):
+class DeepRNN(nn.Module):
     hidden_size: int
     layers: Sequence[Any]
     skip_connections: bool
@@ -97,39 +97,6 @@ class _DeepRNN(RNNCellBase):
         return next_carry, next_states
 
     @staticmethod
-    def initialize_state(
-        num_layers,
-        rng,
-        batch_size: tuple,
-        output_size: int,
-        init_fn=nn.initializers.zeros,
-    ):
-        states = []
-        for i in range(num_layers):
-            print(f"Layer: {i}\n")
-            states.append(
-                _DeepRNN.init_state(
-                    rng=rng,
-                    batch_size=batch_size,
-                    output_size=output_size,
-                    init_fn=init_fn,
-                )
-            )
-
-        return states
-
-    @staticmethod
-    def init_state(
-        rng, batch_size: tuple, output_size: int, init_fn=nn.initializers.zeros
-    ):
-        print(f"batch_size: {(batch_size,)}")
-        print(f"output_size: {(output_size,)}")
-        mem_shape = (batch_size,) + (output_size,)
-        print(f"state mem_shape: {mem_shape}")
-
-        return init_fn(rng, mem_shape)
-
-    @staticmethod
     def initialize_carry(
         rng, batch_size: tuple, hidden_size: int, init_fn=nn.initializers.zeros
     ):
@@ -139,32 +106,3 @@ class _DeepRNN(RNNCellBase):
         print(f"carry mem_shape: {mem_shape}")
 
         return init_fn(rng, mem_shape), init_fn(rng, mem_shape)
-
-
-class DeepRNN(_DeepRNN):
-    r"""Wraps a sequence of cores and callables as a single core.
-        >>> deep_rnn = hk.DeepRNN([
-        ...     LSTMCell(hidden_size=4),
-        ...     jax.nn.relu,
-        ...     LSTMCell(hidden_size=2),
-        ... ])
-    The state of a :class:`DeepRNN` is a tuple with one element per
-    :class:`RNNCore`. If no layers are :class:`RNNCore`\ s, the state is an empty
-    tuple.
-    """
-
-    def __init__(
-        self,
-        hidden_size: int,
-        layers: Sequence[Any],
-        skip_connections: Optional[bool] = False,
-        hidden_to_output_layer: Optional[bool] = False,
-        name: Optional[str] = None,
-    ):
-        super().__init__(
-            hidden_size=hidden_size,
-            layers=layers,
-            skip_connections=skip_connections,
-            hidden_to_output_layer=hidden_to_output_layer,
-            layer_name=name,
-        )
