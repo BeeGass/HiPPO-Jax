1d044de6325980951b1b7c8c69b2d35eab2932a9
Bryan Gass <bryank123@live.com>
2023-02-28 21:26:17 -0500

    preparing to test HiPPO train

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,4 +1,18 @@
 from src.datasets.dataset import Dataset
-from src.datasets.image_dataset import ImageDataset
-from src.datasets.seq_image_dataset import SeqImageDataset
+from src.datasets.mnist import (
+    MNISTDataset,
+    CIFAR10Dataset,
+    CIFAR100Dataset,
+    ImageNetDataset,
+    CocoDataset,
+    CelebADataset,
+)
+from src.datasets.smnist import (
+    sMNISTDataset,
+    sCIFAR10Dataset,
+    sCIFAR100Dataset,
+    sImageNetDataset,
+    sCocoDataset,
+    sCelebADataset,
+)
 from src.datasets.text_dataset import TextDataset
