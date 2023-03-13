1411b8f0cb74d4e37a6869ac5eb0e1fa6a25a2d1
Bryan Gass <bryank123@live.com>
2023-03-13 04:22:02 -0400

    get rid of files that were used as reference, non-functional or are unneeded

--- examples/notebooks/notebooks-jl/lssl.jl
+++
@@ -1,6 +0,0 @@
-function lssl_kernel(sys, t, dt)
-    sys_d = c2d(sys, dt, :tustin)
-    L = convert(Int64, div(t, dt, RoundNearest))
-    z = exp.((-2im * pi) * (0:inv(L):(L-1)/L))
-    return real(ifft([([conj(sys_d.C)] .* ([I] .- ([sys_d.A^L] .* z .^ L)) .* inv.([I] .- ([sys_d.A] .* z)) .* [sys_d.B])...;;], (2,)))
-end
